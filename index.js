652534906494b01d09535ba0f6842dc3f4a39536
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-06-19 11:35:27 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const sinon = require('sinon');
+const { assert } = require('chai');
+
+function writeCards(names, gift) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  })
+
+  afterEach(() => {
+    spy.restore();
+  })
